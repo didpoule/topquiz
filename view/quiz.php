12b6ac6d438edec23166e27defb9e083b7bb152d
Unknown <brito.jordan@icloud.com>
2017-01-30 22:36:57 +0100

    Added display quiz code
    Added code to display a quiz

--- view/quiz.php
+++ view/quiz.php
@@ -1,1 +1,35 @@
-    
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'/>
+    <title>Topquiz</title>
+</head>
+
+<body>
+<div id="corps_page">
+    <form action="quiz.php" id="quiz">
+        <div class="titre"><h1><?= $quiz[0]['titre']?></h1></div>
+        <?php
+        for($question = 0; $question < $nbQuestions; $question++)
+        { ?>
+        <div class="question_quiz">
+            <h2><?= $question+1 . ': '. $quiz[$question]['question']?></h2>
+        </div>
+        <div class="reponses_quiz">
+
+            <?php
+            for($j = 0; $j  < $nbReponses[$question]; $j++)
+            { ?>
+                <p>
+                    <input type="radio" name="question_<?= $question ?>" value="<?= $question.'_'.$j?>">
+                    <?= $reponse[$question][$j] ?>
+                </p>
+                <?php
+            }
+        }
+            ?>
+        </div>
+    </form>
+</div>
+</body>
+</html>
