d1e1ce0bfd8755872e3ba1c7dda8b2dea1c9b8f2
Unknown <brito.jordan@icloud.com>
2017-01-30 20:40:07 +0100

    Database connection set

---
+++ include/sql_connect.php
@@ -0,0 +1,13 @@
+<?php
+$DBHOST = 'localhost';
+$DBNAME = 'topquiz';
+$DBUSER = 'root';
+$DBPASS = '';
+try {
+    $bdd = new PDO("mysql:host=$DBHOST;dbname=$DBNAME", $DBUSER, $DBPASS, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    $setUTF = 'SET CHARACTER SET UTF8;';
+    $query = $bdd->prepare($setUTF);
+    $query->execute();
+} catch (Exception $e) {
+    die('Erreur : ' . $e->getMessage());
+}
