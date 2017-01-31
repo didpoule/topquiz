dcbd207638781e80265e53b4444c42c84b44e9c8
Unknown <brito.jordan@icloud.com>
2017-01-31 19:42:08 +0100

    Added radio selected control function
    Function to check if all radio buttons for a group are selected

---
+++ include/functions.php
@@ -0,0 +1,26 @@
+<?php
+/* Check if radio buttons sent in paramaters are ALL set
+If one button is not set function returns array with button names, else returns false
+*/
+function selectedRadio($nomGroupe, $nbGroupes)
+{
+    $error = array();
+    $j = 0;
+    for ($i = 0; $i < $nbGroupes; $i++)
+    {
+        $fieldName = $nomGroupe . '_' . $i;
+        if (!isset($_POST[$fieldName]))
+        {
+            $error[$j] = $fieldName;
+            $j++;
+        }
+    }
+    if($error)
+    {
+        return $error;
+    }
+    else
+    {
+        return false;
+    }
+}
