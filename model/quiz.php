e7e86e72acc018b7cf43c8e23c6a1fa3910fe4c2
Unknown <brito.jordan@icloud.com>
2017-01-31 19:44:32 +0100

    Edited SQL query function getQuiz
    Added SELECT Quiz.id

--- model/quiz.php
+++ model/quiz.php
@@ -4,12 +4,13 @@ function getQuiz($id)
     global $bdd;
 
     $req = $bdd->prepare( 'SELECT Quiz.nom AS titre, q.contenu AS question, GROUP_CONCAT(r.contenu ORDER BY r.id) AS reponses, 
-                                    COUNT(DISTINCT q.id) AS nb_questions, COUNT(DISTINCT r.id) AS nb_reponses
+                                    COUNT(DISTINCT q.id) AS nb_questions, COUNT(DISTINCT r.id) AS nb_reponses,
+                                    Quiz.id AS id
                                     FROM Quiz 
                                     INNER JOIN Question AS q ON q.id_quiz = Quiz.id
                                     INNER JOIN Reponse AS r ON r.id_question = q.id
                                     WHERE Quiz.id = :quiz
-                                    GROUP BY titre, question WITH ROLLUP
+                                    GROUP BY id, titre, question WITH ROLLUP
                                     ');
     $req->bindParam(':quiz', $id, PDO::PARAM_INT);
     $req->execute();
