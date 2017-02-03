e20279cd2784e4bbbe1e1e0075fc4982ce882975
Unknown <brito.jordan@icloud.com>
2017-02-03 14:28:46 +0100

    HTML tags on php conditions added
    now answers have different HTML tag thanks to php condition

--- view/quiz_result.php
+++ view/quiz_result.php
@@ -11,34 +11,44 @@
     <?php
     for($question = 0; $question < $nbQuestions; $question++)
     {
-
         ?>
     <div class="question_quiz">
         <h2><?= $question+1 . ': '. $quiz[$question]['question']?></h2>
     </div>
     <div class="reponses_quiz">
-
         <?php
         for($j = 0; $j  < $nbReponses[$question]; $j++)
         {
-            if ($reponse[$question]['contenu'][$j] != $repChoisies[$question]['contenu'])
-            {
-                echo '<div class="wrong">';
-            } else
+            /* Si la réponse à afficher est celle choisie par l'utilisateur et qu'elle est juste
+                OU Si la réponse à afficher est la bonne réponse et non choisie par l'utilisateur*/
+            if (($reponse[$question]['contenu'][$j] == $repChoisies[$question]['contenu']) &&
+                $repChoisies[$question]['reponse_id'] == $correction[$question]['reponse_id'] ||
+                (($correction[$question]['contenu'] == $reponse[$question]['contenu'][$j]) &&
+                    $repChoisies[$question]['reponse_id'] !=  $correction[$question]['reponse_id']))
             {
                 echo '<div class="right">';
+                // Si la réponse à afficher est celle choisie par l'utilisateur et qu'elle est fausse
+            } elseif(($reponse[$question]['contenu'][$j] == $repChoisies[$question]['contenu']) &&
+                $repChoisies[$question]['reponse_id'] != $correction[$question]['reponse_id'])
+            {
+                echo '<div class="wrong">';         
+            } else
+            {
+                echo '<div class="neutre">';
             }
+
             ?>
 
             <p>
                 <?= $reponse[$question]['contenu'][$j]//$reponse[$question][$j]  ?>
             </p>
+            </div>
             <?php
         }
     }
         ?>
-    </div>
 </div>
+<h1>Score: <?= $score['score'] ?> %</h1>
 </div>
 </body>
 </html>
