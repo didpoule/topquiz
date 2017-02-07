3c436fa4bd62b1349b01352be3171d9211865044
Unknown <brito.jordan@icloud.com>
2017-02-07 11:54:36 +0100

    Revert "Useless brackets have been deleted"
    This reverts commit 0d94569cee354f353f3a9bb20f6e53a71fd10c77.

--- include/functions.php
+++ include/functions.php
@@ -1,37 +1,10 @@
 <?php
-function setQuizArray($donnees)
-{
-    if ($donnees) {
-        $nbQuestions = 0;
-        $nbReponses = array();
-        $reponse = array();
-        foreach ($donnees as $cle => $donnees[$nbQuestions]) {
-            $nbReponses[$nbQuestions] = $donnees[$cle]['nb_reponses'];
-            $nbQuestions++;
-        }
-        for ($i = 0; $i < $nbQuestions; $i++) {
-            $reponse[$i]['contenu'] = explode(',', $donnees[$i]['reponses']);
-            $reponse[$i]['id'] = explode(',', $donnees[$i]['reponses_id']);
-        }
-        $quiz['quiz_infos']['titre'] = $donnees[0]['titre'];
-        $quiz['quiz_infos']['id'] = $donnees[0]['id'];
-        $quiz['quiz_infos']['nombre_questions'] = $nbQuestions;
-        for ($i = 0; $i < $nbQuestions; $i++) {
-            $quiz['question_' . $i]['question_contenu'] = $donnees[$i]['question'];
-            $quiz['question_' . $i]['question_id'] = $donnees[$i]['question_id'];
-            $quiz['question_' . $i]['nombre_reponses'] = $donnees[$i]['nb_reponses'];
-            for ($j = 0; $j < $nbReponses[$i]; $j++) {
-                $quiz['question_' . $i]['reponses_contenu'][$j] = $reponse[$i]['contenu'][$j];
-                $quiz['question_' . $i]['reponses_id'][$j] = $reponse[$i]['id'][$j];
-            }
-        }
-        return $quiz;
-    }
-}
-
-/* Check if radio buttons sent in paramaters are ALL set
-If one button is not set function returns array with button names, else returns false
-*/
+/**
+ * @param $nomGroupe // Field's prefixe name
+ * @param $nbGroupes // Number of fields
+ * @return array|bool // returns array with button names, else returns false
+ *  Check if every field is set
+ */
 function selectedRadio($nomGroupe, $nbGroupes)
 {
     $error = array();
