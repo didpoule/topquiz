08759bfbf0ee885a1bdb0a43fb194df7160921b5
Unknown <brito.jordan@icloud.com>
2017-02-10 21:21:57 +0100

    User can send as many quiz form he wants
    v2 update was also applied here, view commit ed7047bec96f2768968a04429c0a14ad08eada5f

--- controller/quiz.php
+++ controller/quiz.php
@@ -14,44 +14,52 @@ if (isset($_GET['quiz']) && !isset($_POST['envoyer'])) {
     }
 }
 if (isset($_POST['envoyer'])) {
-    if (isset($_POST['id_quiz']) && isset($_GET['quiz'])) {
-        $quiz_id = (int)$_POST['id_quiz'];
-        $quiz = getQuiz($quiz_id);
-        $quiz = setQuizArray($quiz);
-        $nbQuestions = $quiz['quiz_infos']['nombre_questions'];
-        if ($_POST['id_quiz'] === $_GET['quiz']) {
-            $error = selectedRadio('question', $nbQuestions);
-        }
-    }
-    if ($error) {
-        header('Location: ' . $_SERVER['HTTP_REFERER']);
-    } else {
-        $correction = getBonnesReponses($_POST['id_quiz']);
-        foreach($correction as $k => $v) {
-            if($_POST['question_type_' . $k] == 1)
-            {
-                $correction[$k]['contenu'] = explode(',', $v['contenu']);
+    $quiz_id = (int)$_POST['id_quiz'];
+    $quiz = getQuiz($quiz_id);
+    $quiz = setQuizArray($quiz);
+    $nbQuestions = $quiz['quiz_infos']['nombre_questions'];
+    if ($_SESSION['is_connected']) {
+            if (isset($_POST['id_quiz']) && isset($_GET['quiz'])) {
+                $quiz_id = (int)$_POST['id_quiz'];
+                $quiz = getQuiz($quiz_id);
+                $quiz = setQuizArray($quiz);
+                $nbQuestions = $quiz['quiz_infos']['nombre_questions'];
+                if ($_POST['id_quiz'] === $_GET['quiz']) {
+                    $error = selectedRadio('question', $nbQuestions);
+                }
             }
-        }
-        for ($question = 0; $question < $nbQuestions; $question++) {
-            $repChoisies['question_id'][$question] = $_POST['question_id_' . $question];
+
+        if ($error) {
+            header('Location: ' . $_SERVER['HTTP_REFERER']);
+        } else {
+            $correction = getBonnesReponses($_POST['id_quiz']);
+            foreach ($correction as $k => $v) {
+                if ($_POST['question_type_' . $k] == 1) {
+                    $correction[$k]['contenu'] = explode(',', $v['contenu']);
+                }
+            }
+            for ($question = 0; $question < $nbQuestions; $question++) {
+                $repChoisies['question_id'][$question] = $_POST['question_id_' . $question];
                 $repChoisies['reponse']['contenu'][$question] = $_POST['question_' . $question];
-        }
-        $score = quizScore($nbQuestions, $repChoisies, $correction);
-        $idRep = getRepId($repChoisies['reponse']['contenu']);
-        foreach ($idRep as $key => $value) {
-            if($_POST['question_type_' . $key] == 1) {
-                $repChoisies['reponse']['id'][$key] = explode(',', $value['reponse_id']);
-            } else {
-                $repChoisies['reponse']['id'][$key] = $value['reponse_id'];
             }
-        }
-        if ($_SESSION['is_connected']) {
+            $score = quizScore($nbQuestions, $repChoisies, $correction);
+            $idRep = getRepId($repChoisies['reponse']['contenu']);
+            foreach ($idRep as $key => $value) {
+                if ($_POST['question_type_' . $key] == 1) {
+                    $repChoisies['reponse']['id'][$key] = explode(',', $value['reponse_id']);
+                } else {
+                    $repChoisies['reponse']['id'][$key] = $value['reponse_id'];
+                }
+            }
             if(!in_array($quiz_id, $_SESSION['quiz_done'])) {
                 add_quizToUser($quiz_id, $_SESSION['user_id']);
                 update_user_quiz();
             }
+
+            require_once('view/quiz_result.php');
         }
-        require_once('view/quiz_result.php');
+    } else {
+        $msg = 'Vous devez être connecté pour faire un quiz';
+        require_once('view/quiz.php');
     }
 }
