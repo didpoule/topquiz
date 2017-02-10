8a328e1ec6c140917454b3daecf3d29fb0c5155c
Unknown <brito.jordan@icloud.com>
2017-02-10 21:04:20 +0100

    Added a function to init $_SESSION
    Now $_SESSSION vars are initlialized if not set

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 session_start();
-if(!isset($_SESSION['is_connected'])) {
-    $_SESSION['is_connected'] = 0;
-}
 require_once('include/sql.php');
 require_once('include/functions.php');
 require_once('include/user_functions.php');
+if(!isset($_SESSION['is_connected'])) {
+    init_user();
+}
 if (isset($_GET['section'])) {
     $section = htmlspecialchars($_GET['section']);
     $filename = 'controller/' . $section . '.php';
